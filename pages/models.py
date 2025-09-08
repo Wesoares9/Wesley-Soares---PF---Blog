b7472de24de9fdf6b391928358f3c929140103bf
Wesoares9 <wesley.henrique.soares@usp.br>
2025-09-08 20:44:41 -0300

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)            # CharField 1
-    subtitle = models.CharField(max_length=200, blank=True)  # CharField 2
-    content = RichTextField()                           # texto enriquecido (CKEditor)
+    title = models.CharField(max_length=200)            
+    subtitle = models.CharField(max_length=200, blank=True) 
+    content = RichTextField()                         
     image = models.ImageField(upload_to='posts/%Y/%m/%d/', blank=True, null=True)
-    published_at = models.DateTimeField(auto_now_add=True)  # data
+    published_at = models.DateTimeField(auto_now_add=True) 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     
 
