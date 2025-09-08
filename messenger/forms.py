a4a5ea5b875d7989f184935b976c47e8f15508cd
Wesoares9 <wesley.henrique.soares@usp.br>
2025-09-08 20:41:18 -0300

    Update forms.py

--- messenger/forms.py
+++ messenger/forms.py
@@ -20,21 +20,17 @@ class MessageForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-        """
-        Recebe `user=` ao criar a instância (na view). Usamos esse user
-        para remover o próprio usuário da lista de destinatários.
-        """
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
-        # Se temos o user, exclui-o do queryset de destinatários
+      
         if self.user is not None:
             self.fields['receiver'].queryset = User.objects.exclude(pk=self.user.pk).order_by('username')
         else:
             self.fields['receiver'].queryset = User.objects.all().order_by('username')
 
     def clean_receiver(self):
-        ##Evita envio para si mesmo caso alguém tente manipular o formulário.
+        
         receiver = self.cleaned_data.get('receiver')
         if self.user and receiver == self.user:
             raise forms.ValidationError('Você não pode enviar uma mensagem para si mesmo.')
